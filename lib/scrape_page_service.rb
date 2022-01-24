9389b3e0c80e1dec892ab891a6fa7f1e98429748
shaharyar-qbatch <m.shaharyar@qbatch.com>
2022-01-24 12:08:24 +0500

    first commit

---
+++ lib/scrape_page_service.rb
@@ -0,0 +1,63 @@
+require "mechanize"
+
+class ScrapePageService
+
+  def initialize
+    @agents = File.readlines("user_agents.txt")
+  end
+
+  def get_agent_object(read_timeout, open_timeout, idle_timeout)
+    agent = Mechanize.new
+    agent.read_timeout = read_timeout
+    agent.open_timeout = open_timeout
+    agent.keep_alive = false
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    agent.idle_timeout = idle_timeout
+    agent.pluggable_parser.default = Mechanize::Page
+    agent
+  end
+  
+  def set_agent_proxy(agent, ip, port, username, password)
+    agent.user_agent =  @agents.sample.strip
+    agent.set_proxy(ip, port, username, password)
+  end
+
+
+  def send_request(**kwargs)
+    agent = get_agent_object(kwargs[:read_timeout], kwargs[:open_timeout], kwargs[:idle_timeout])
+    tries = 0
+    max_tries = kwargs[:max_retries]
+    set_agent_proxy(agent, kwargs[:ip], kwargs[:port], kwargs[:username], kwargs[:password])
+    begin
+      puts "Start request #{Time.now}"
+      puts "Url is: #{kwargs[:url]}"
+      page = kwargs[:params].nil? ? agent.get(kwargs[:url], nil, kwargs[:headers]) : agent.post(kwargs[:url], kwargs[:params], kwargs[:headers])
+      puts "End request #{Time.now}"
+      puts "Found"
+    rescue Mechanize::ResponseCodeError => ex
+      puts "Response error: HTTP #{ex.response_code}, proxy: #{agent.proxy_addr}, End Request: #{Time.now}"
+      refresh_agent(agent)
+      set_agent_proxy(agent, kwargs[:ip], kwargs[:port], kwargs[:username], kwargs[:password])
+      tries += 1
+      retry if tries < max_tries
+    rescue Exception => ex
+      puts "Message: #{ex.message}, proxy: #{agent.proxy_addr}, End Request: #{Time.now}"
+      refresh_agent(agent)
+      set_agent_proxy(agent, kwargs[:ip], kwargs[:port], kwargs[:username], kwargs[:password])
+      if (ex.message.include?("Verify your identity") || ex.message.include?("Robot or human"))
+        retry
+      else
+        tries += 1
+        retry if tries < max_tries
+      end
+    end
+    return page
+  end
+
+  def refresh_agent(agent)
+    agent.cookie_jar.clear!
+    agent.history.clear
+  end
+
+
+end
---
+++ scrape_page_service.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.version     = '0.0.0'
+  s.summary     = "page!"
+  s.name        = 'scrape_page_service'
+  s.authors     = ["Muhammad Shaharyar"]
+  s.files       = ["lib/scrape_page_service.rb"]
+  s.email       = 'muhammadshaharyar009@gmail.com'
+  s.description = "Get the response successfully by hiting any link through Http requests"
+  # s.homepage    =
+  #   'https://rubygems.org/gems/hola'
+  # s.license       = 'MIT'
+end
